6adc0a49ca98331693dc6a692c41857b3410981f
Abbygaile Blue Calitis <calitisabby@gmail.com>
2024-05-23 22:10:01 +0800

    design revisions

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -1,24 +1,24 @@
 <x-layout>
     <h1 class="text-3xl font-bold mt-4">Post Edit Page</h1>
-    <div class="max-w-2xl mx-auto p-4 bg-slate-200 rounded-lg">
+    <div class="max-w-2xl mx-auto p-4 bg-white rounded-lg shadow-lg">
         <form action="{{ route('posts.update', $post->id) }}" method="POST">
             @csrf
             @method('PUT')
             <div class="mb-6">
                 <label for="large-input"
-                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">TItle</label>
+                    class="block mb-2 text-sm font-medium text-gray-900">Title</label>
                 <input type="text" id="large-input" name="title" placeholder="Title"
                     value="{{ old('title', $post->title) }}"
-                    class="@error('title') border-red-500 @enderror block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                    class="@error('title') border-red-500 @enderror block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-100 focus:ring-blue-500 focus:border-blue-500">
                 @error('title')
                     <span class="text-red-500 text-sm">{{ $message }}</span>
                 @enderror
             </div>
             <div class="mb-6">
                 <label for="message"
-                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Content</label>
+                    class="block mb-2 text-sm font-medium text-gray-900">Content</label>
                 <textarea id="message" rows="4" name="content"
-                    class="@error('content') border-red-500 @enderror block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    class="@error('content') border-red-500 @enderror block p-2.5 w-full text-sm text-gray-900 bg-gray-100 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
                     placeholder="Write your content here...">{{ old('content', $post->content) }}</textarea>
                 @error('content')
                     <span class="text-red-500 text-sm">{{ $message }}</span>
@@ -26,7 +26,7 @@
             </div>
             <div class="mb-6">
                 <button type="submit"
-                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Update</button>
+                    class="text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none">Update</button>
             </div>
         </form>
     </div>
